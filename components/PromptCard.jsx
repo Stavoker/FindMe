3235521baa29fb89d84f902fb784ec8ef4e88750
Rostyslav Sikora <166741724+Stavoker@users.noreply.github.com>
2024-10-14 18:01:34 +0100

    Update PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { useUser } from "@clerk/nextjs"; // Clerk's user hook
 import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
 
 const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { user } = useUser(); // Get current user from Clerk
@@ -45,19 +46,21 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   };
 
   return (
-      <div className="prompt_card">
-        <div className="flex justify-between items-start gap-5">
+    <div className="prompt_card">
+
+      <div className="flex justify-between items-start gap-5">
+        <Link href="/user-profile">
           <div
-              className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
-              onClick={handleProfileClick}
+            className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+            onClick={handleProfileClick}
           >
             {/* Use creatorUser's image if it's available */}
             <div className="relative w-10 h-10 rounded-full overflow-hidden">
               <Image
-                  src={creatorUser?.imageUrl || "/assets/icons/default-avatar.svg"}
-                  alt="user_image"
-                  layout="fill"
-                  objectFit="cover" // Ensures the image covers the container
+                src={creatorUser?.imageUrl || "/assets/icons/default-avatar.svg"}
+                alt="user_image"
+                layout="fill"
+                objectFit="cover" // Ensures the image covers the container
               />
             </div>
 
@@ -70,47 +73,48 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
               </p>
             </div>
           </div>
-
-          <div className="copy_btn" onClick={handleCopy}>
-            <Image
-                src={
-                  copied === post.prompt
-                      ? "/assets/icons/tick.svg"
-                      : "/assets/icons/copy.svg"
-                }
-                alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
-                width={12}
-                height={12}
-            />
-          </div>
+        </Link>
+        <div className="copy_btn" onClick={handleCopy}>
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+            width={12}
+            height={12}
+          />
         </div>
 
-        <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-        <p
-            className="font-inter text-sm blue_gradient cursor-pointer"
-            onClick={() => handleTagClick && handleTagClick(post.tag)}
-        >
-          {post.tag.join(" ")}
-        </p>
-
-        {/* Check if the current user is the owner of the post */}
-        {user?._id === post?.creator?._id && pathName === "/profile" && (
-            <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-              <p
-                  className="font-inter text-sm green_gradient cursor-pointer"
-                  onClick={handleEdit}
-              >
-                Edit
-              </p>
-              <p
-                  className="font-inter text-sm orange_gradient cursor-pointer"
-                  onClick={handleDelete}
-              >
-                Delete
-              </p>
-            </div>
-        )}
       </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag.join(" ")}
+      </p>
+
+      {/* Check if the current user is the owner of the post */}
+      {user?._id === post?.creator?._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
+    </div>
   );
 };
 
